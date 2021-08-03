6035ea8c36c892555b8f0f7cdbd24b1efe0973c5
Eduard <jimlab@mail.ru>
2021-08-03 20:07:19 +0300

    to BasePage add def is_element_present, to MainPage add def should_be_login_link, add new test to check login

--- test_main_page.py
+++ test_main_page.py
@@ -1,5 +1,13 @@
+from .pages.main_page import MainPage
+
+
+
 def test_guest_can_go_to_login_page(browser):
     link = "http://selenium1py.pythonanywhere.com/"
-    browser.get(link)
-    login_link = browser.find_element_by_css_selector("#login_link")
-    login_link.click()
+    page = MainPage(browser, link) # init Page Object, with init BasePage(browser, url)
+    page.open() # as browser.get(url) open page
+    page.go_to_login_page() # find css selector and click, go to login page
+    page.should_be_login_link() # check login link
+
+
+# pytest -v --tb=line --language=en test_main_page.py
