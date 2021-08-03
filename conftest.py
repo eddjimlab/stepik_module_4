a5c9eff9db52edf53b597a2a4eda0b1675da1228
Eduard <jimlab@mail.ru>
2021-08-03 16:27:49 +0300

    add test_main_page.py, SETUP enviroments

--- conftest.py
+++ conftest.py
@@ -5,7 +5,7 @@ from selenium.webdriver.chrome.options import Options
 
 
 def pytest_addoption(parser):
-    parser.addoption('--language', action='store', default=None, help="Choose language")
+    parser.addoption('--language', action='store', default="en", help="Choose language")
 
 
 
