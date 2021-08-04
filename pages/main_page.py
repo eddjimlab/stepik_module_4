29afea6c46806d29e85db70fa6f91fd7df9f5457
Eduard <jimlab@mail.ru>
2021-08-04 15:31:39 +0300

    move self.browser.current_url from LoginPage class to test_main_page.py login_page = LoginPage(browser, browser.current_url)

--- pages/main_page.py
+++ pages/main_page.py
@@ -1,5 +1,4 @@
 from .base_page import BasePage
-from selenium.webdriver.common.by import By
 from .locators import MainPageLocators
 
 
@@ -9,4 +8,4 @@ class MainPage(BasePage):
         login_link.click()
 
     def should_be_login_link(self):
-         assert self.is_element_present(*MainPageLocators.LOGIN_LINK), "Login link is not presented"
+        assert self.is_element_present(*MainPageLocators.LOGIN_LINK), "Login link is not presented"
--- test_main_page.py
+++ test_main_page.py
@@ -2,17 +2,15 @@ from .pages.main_page import MainPage
 from .pages.login_page import LoginPage
 
 
-
 def test_guest_can_go_to_login_page(browser):
     link = "http://selenium1py.pythonanywhere.com/"
     link2 = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209?promo=midsummer "
-    page = MainPage(browser, link) # init Page Object, with init BasePage(browser, url)
-    page.open() # as browser.get(url) open page
-    page.go_to_login_page() # find css selector and click, go to login page
-    page.should_be_login_link() # check login link
+    page = MainPage(browser, link)  # init Page Object, with init BasePage(browser, url)
+    page.open()  # as browser.get(url) open page
+    page.should_be_login_link()  # check login link
+    page.go_to_login_page()  # find css selector and click, go to login page
 
-    login_page = LoginPage(browser, link)
+    login_page = LoginPage(browser, browser.current_url)
     login_page.should_be_login_page()
-
 
 # pytest -v --tb=line --language=en test_main_page.py
