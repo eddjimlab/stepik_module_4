b8198a912a4faea614338a0bbafb13627e006b58
Eduard <jimlab@mail.ru>
2021-08-06 18:49:49 +0300

    test promo links, mark xfailed bad link

--- test_product_page.py
+++ test_product_page.py
@@ -1,12 +1,19 @@
+import pytest
+
 from .pages.product_page import PageObject
 
+base_link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer"
 
-def test_guest_can_add_product_to_basket(browser):
-    link = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/?promo=newYear"
+
+@pytest.mark.parametrize('link', [f'{base_link}0', f'{base_link}1', f'{base_link}2', f'{base_link}3', f'{base_link}4',
+                                  f'{base_link}5', f'{base_link}6',
+                                  pytest.param(f'{base_link}7', marks=pytest.mark.xfail(reason="bad link")),
+                                  f'{base_link}8', f'{base_link}9'])
+def test_guest_can_add_product_to_basket(browser, link):
+    link0 = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/?promo=newYear"
+    link2 = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=newYear2019"
     product_page = PageObject(browser, link)
     product_page.open()
     product_page.add_product_to_cart()
-    product_page.product_added_and_basket_equal() # test added and cart product name
-    product_page.price_added_and_basket_equal() # test price in cart and added is equal
-
-
+    product_page.product_added_and_basket_equal()  # test added and cart product name
+    product_page.price_added_and_basket_equal()  # test price in cart and added is equal
